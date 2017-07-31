0ebe73bce7aadfd01211e6cccf441c50c93fe13c
Mohamed GAFSIA <mohamed.gafsia_sidifrance@ca-caagis.fr>
2017-07-31 15:05:15 +0200

    Add angular animation cases (3).

--- angBinding-web/src/main/angBinding-web/src/animations/animation.component.ts
+++ angBinding-web/src/main/angBinding-web/src/animations/animation.component.ts
@@ -1,17 +1,29 @@
 import {Component, OnInit} from "@angular/core";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'mga-anim',
-  templateUrl: 'animation.component.html'
+  templateUrl: 'animation.component.html',
+  animations: [ trigger('theTextDiv', [
+                        state('open', style({ 'height' : '*'})),
+                        state('closed', style({ 'height' : '0px', 'font-size' : '0px'})),
+                        transition('closed <=> open', [animate(1000)])
+                        ])
+              ]
+
 })
 export class AnimationComponent implements OnInit{
-  hideShowLabel: string = "Hide";
+  hideShow: boolean = true;
+  theTextDiv: string;
 
   ngOnInit(){
   }
 
   hideShowClick() {
-    if(this.hideShowLabel == "Hide") this.hideShowLabel = "Show";
-    else this.hideShowLabel = "Hide";
+    this.hideShow = !this.hideShow;
+    console.log(this.hideShow);
+
+    if(this.hideShow) this.theTextDiv = 'open';
+    if(this.hideShow) this.theTextDiv = 'closed';
   }
 }
